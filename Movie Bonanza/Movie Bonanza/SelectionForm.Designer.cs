b5a831b88a7b88ab26751c99ab6e6426c78a0dbf
unknown <dbbarcenas@gmail.com>
2013-11-16 16:04:08 -0500

    Version 1.0

---
+++ Movie Bonanza/Movie Bonanza/SelectionForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Movie_Bonanza
+{
+    partial class SelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MoviesListBox = new System.Windows.Forms.ListBox();
+            this.CurrentMoviesLabel = new System.Windows.Forms.Label();
+            this.YourSelectionGroupBox = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.CategoryListBox = new System.Windows.Forms.ListBox();
+            this.CategoryLbl = new System.Windows.Forms.Label();
+            this.YourSelectionGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoviesListBox
+            // 
+            this.MoviesListBox.FormattingEnabled = true;
+            this.MoviesListBox.Location = new System.Drawing.Point(114, 69);
+            this.MoviesListBox.Name = "MoviesListBox";
+            this.MoviesListBox.Size = new System.Drawing.Size(168, 199);
+            this.MoviesListBox.Sorted = true;
+            this.MoviesListBox.TabIndex = 0;
+            this.MoviesListBox.Click += new System.EventHandler(this.MoviesListBox_SelectedIndexChanged);
+            this.MoviesListBox.SelectedIndexChanged += new System.EventHandler(this.MoviesListBox_SelectedIndexChanged);
+            // 
+            // CurrentMoviesLabel
+            // 
+            this.CurrentMoviesLabel.AutoSize = true;
+            this.CurrentMoviesLabel.Location = new System.Drawing.Point(111, 53);
+            this.CurrentMoviesLabel.Name = "CurrentMoviesLabel";
+            this.CurrentMoviesLabel.Size = new System.Drawing.Size(78, 13);
+            this.CurrentMoviesLabel.TabIndex = 1;
+            this.CurrentMoviesLabel.Text = "Current Movies";
+            // 
+            // YourSelectionGroupBox
+            // 
+            this.YourSelectionGroupBox.Controls.Add(this.pictureBox1);
+            this.YourSelectionGroupBox.Controls.Add(this.TitleLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.CostLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.CategoryLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.textBox3);
+            this.YourSelectionGroupBox.Controls.Add(this.textBox2);
+            this.YourSelectionGroupBox.Controls.Add(this.textBox1);
+            this.YourSelectionGroupBox.Location = new System.Drawing.Point(305, 69);
+            this.YourSelectionGroupBox.Name = "YourSelectionGroupBox";
+            this.YourSelectionGroupBox.Size = new System.Drawing.Size(382, 170);
+            this.YourSelectionGroupBox.TabIndex = 2;
+            this.YourSelectionGroupBox.TabStop = false;
+            this.YourSelectionGroupBox.Text = "Your Selection";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 129);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(157, 26);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(27, 13);
+            this.TitleLabel.TabIndex = 5;
+            this.TitleLabel.Text = "Title";
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(321, 71);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(28, 13);
+            this.CostLabel.TabIndex = 4;
+            this.CostLabel.Text = "Cost";
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Location = new System.Drawing.Point(157, 71);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.CategoryLabel.TabIndex = 3;
+            this.CategoryLabel.Text = "Category";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(314, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(53, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(151, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose the movies you wish to STREAM from the list below";
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(612, 245);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 4;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // CategoryListBox
+            // 
+            this.CategoryListBox.FormattingEnabled = true;
+            this.CategoryListBox.Items.AddRange(new object[] {
+            "Comedy ",
+            "Drama",
+            "Action",
+            "Sci-Fi",
+            "Horror",
+            "Thriller",
+            "Family",
+            "New Release"});
+            this.CategoryListBox.Location = new System.Drawing.Point(13, 69);
+            this.CategoryListBox.Name = "CategoryListBox";
+            this.CategoryListBox.Size = new System.Drawing.Size(76, 108);
+            this.CategoryListBox.TabIndex = 5;
+            this.CategoryListBox.Click += new System.EventHandler(this.MoviesListBox_SelectedIndexChanged);
+            this.CategoryListBox.SelectedIndexChanged += new System.EventHandler(this.CategoryListBox_SelectedIndexChanged);
+            // 
+            // CategoryLbl
+            // 
+            this.CategoryLbl.AutoSize = true;
+            this.CategoryLbl.Location = new System.Drawing.Point(10, 53);
+            this.CategoryLbl.Name = "CategoryLbl";
+            this.CategoryLbl.Size = new System.Drawing.Size(49, 13);
+            this.CategoryLbl.TabIndex = 6;
+            this.CategoryLbl.Text = "Category";
+            // 
+            // SelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 280);
+            this.ControlBox = false;
+            this.Controls.Add(this.CategoryLbl);
+            this.Controls.Add(this.CategoryListBox);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.YourSelectionGroupBox);
+            this.Controls.Add(this.CurrentMoviesLabel);
+            this.Controls.Add(this.MoviesListBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectionForm";
+            this.ShowIcon = false;
+            this.Text = "Selection";
+            this.YourSelectionGroupBox.ResumeLayout(false);
+            this.YourSelectionGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox MoviesListBox;
+        private System.Windows.Forms.Label CurrentMoviesLabel;
+        private System.Windows.Forms.GroupBox YourSelectionGroupBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.ListBox CategoryListBox;
+        private System.Windows.Forms.Label CategoryLbl;
+    }
+}
+
---
+++ Movie Bonanza/Movie Bonanza/SelectionForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Splashscreen threading 
+using System.Threading; 
+namespace Movie_Bonanza
+{
+    public partial class SelectionForm : Form
+    {
+        decimal[,] DownloadCost = new decimal[,] 
+        { {1.99m, 1.99m, 2.99m, 2.99m, 2.99m, 1.99m, 0.99m, 4.99m } };
+
+        string[] category = new string[] {"Comedy" };
+
+        string[] comedyMovies = new string[] { "The Delemma", "No Strings Attached", "Cedar Rapids", "Just Go With it" };
+        string[] dramaMovies = new string[] {"Company Men", "The Way Back", "Waiting for Forever"  };
+        string[] actionMovies = new string[] {"The Green Hornet", "Death Race 2", "The Mechanic", "Sanctum", "The Other Woman", "The Eagle" };
+        string[] scifiMovies = new string[] {"Season of the Witch", "I am Number Four" };
+        string[] horrorMovies = new string[] {"The Rite"};
+        string[] thrillerMovies = new string[] {"The Roomate" };
+        string[] familyMovies = new string[] {"Gnomeo and Juliet" };
+        string[] newReleaseMovies = new string[] {"Footloose", "Real Steel" }; 
+
+
+        public SelectionForm()
+        {
+            //to be added splashscreen
+            Thread t = new Thread(new ThreadStart(SplashStart));
+            t.Start();
+            Thread.Sleep(5000);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        /*
+        * funciton for splashstart to be called 
+        * */
+        public void SplashStart()
+        {
+            Application.Run(new SplashScreenForm());
+        }
+
+        private void MoviesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int comedyIndexInteger, dramaIndexInteger, actioinIndexInteger, scifiIndexInteger, horrorIndexInteger, thrillerIndexInteger,
+                familyIndexInteger, newReleaseIndexInteger = MoviesListBox.SelectedIndex; 
+        }
+
+        private void CategoryListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int categoryIndexInteger = CategoryListBox.SelectedIndex;
+            if (categoryIndexInteger == 0)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(comedyMovies);
+            }
+            if (categoryIndexInteger == 1) {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(dramaMovies); 
+            }
+            if (categoryIndexInteger == 2)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(actionMovies);
+            }
+            if (categoryIndexInteger == 3)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(scifiMovies);
+            }
+            if (categoryIndexInteger == 4)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(horrorMovies);
+            }
+            if (categoryIndexInteger == 5)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(thrillerMovies);
+            }
+            if (categoryIndexInteger == 6)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(familyMovies);
+            }
+            if (categoryIndexInteger == 7)
+            {
+                MoviesListBox.Items.Clear(); 
+                MoviesListBox.Items.AddRange(newReleaseMovies);
+            }
+        }
+    }
+}
